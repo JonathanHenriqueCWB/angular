c27f8b83b2434d1f3432c6c72ed4fb5c14840d1e
JonathanLima <jonathanhenriquecwb@gmail.com>
2020-02-22 09:50:33 -0300

    ngIf e ngFor

--- primeiroProjeto/src/app/app-routing.module.ts
+++ primeiroProjeto/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { NgIfNgForComponent } from './ng-if-ng-for/ng-if-ng-for.component';
 
 
 const routes: Routes = [
   {path: 'primeiro-componente', component: PrimeiroComponenteComponent},
   {path: 'segundo-componente', component: SegundoComponenteComponent},
-  {path: 'terceiro-componente', component: TerceiroComponenteComponent}
+  {path: 'terceiro-componente', component: TerceiroComponenteComponent},
+  {path: 'teste-componente', component: NgIfNgForComponent}
 ];
 
 @NgModule({
--- primeiroProjeto/src/app/app.module.ts
+++ primeiroProjeto/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 import { TerceiroComponenteComponent } from './terceiro-componente/terceiro-componente.component';
+import { NgIfNgForComponent } from './ng-if-ng-for/ng-if-ng-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiroComponenteComponent,
     SegundoComponenteComponent,
-    TerceiroComponenteComponent
+    TerceiroComponenteComponent,
+    NgIfNgForComponent
   ],
   imports: [
     BrowserModule,
