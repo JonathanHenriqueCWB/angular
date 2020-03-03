0a98a10d8c63f6696bf2b7a0ce0f4c8452f8dc20
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-03-03 10:10:49 -0300

    Modulos

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -7,17 +7,21 @@ import { AppComponent } from './app.component';
 // Importando modulo from 'nomeComponente/nomeArquivo'
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { CursosModule } from './cursos/cursos.module';
+import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // Add modulo
     PrimeiroComponenteComponent,
-    SegundoComponenteComponent
+    SegundoComponenteComponent,
+    CursosDetalheComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
