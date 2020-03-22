c98d51e4c93bf8fc9f1f27e8fe2b3e0c97ba3ee0
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-03-21 22:58:21 -0300

    Modulos

---
+++ modulos/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { CursosModule } from './cursos/cursos.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CursosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
